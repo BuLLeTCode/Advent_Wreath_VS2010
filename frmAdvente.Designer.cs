92782ac477f677937e1d5c5e8d700782478d0204
BuLLeT1331 <gta122@inbox.lv>
2015-12-28 23:53:44 +0200

    Advent_Project_Visual_Studio_2010

---
+++ frmAdvente.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AdventesKalendars
+{
+    partial class frmAdvente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbAdventeCandle1 = new System.Windows.Forms.PictureBox();
+            this.pbAdvente = new System.Windows.Forms.PictureBox();
+            this.pbAdventeCandle2 = new System.Windows.Forms.PictureBox();
+            this.pbAdventeCandle3 = new System.Windows.Forms.PictureBox();
+            this.pbAdventeCandle4 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdvente)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAdventeCandle1
+            // 
+            this.pbAdventeCandle1.BackColor = System.Drawing.Color.Transparent;
+            this.pbAdventeCandle1.Location = new System.Drawing.Point(0, 216);
+            this.pbAdventeCandle1.Name = "pbAdventeCandle1";
+            this.pbAdventeCandle1.Size = new System.Drawing.Size(129, 112);
+            this.pbAdventeCandle1.TabIndex = 1;
+            this.pbAdventeCandle1.TabStop = false;
+            // 
+            // pbAdvente
+            // 
+            this.pbAdvente.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbAdvente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbAdvente.Image = global::AdventesKalendars.Properties.Resources.wreath_23374_640;
+            this.pbAdvente.Location = new System.Drawing.Point(0, 0);
+            this.pbAdvente.Name = "pbAdvente";
+            this.pbAdvente.Size = new System.Drawing.Size(628, 582);
+            this.pbAdvente.TabIndex = 0;
+            this.pbAdvente.TabStop = false;
+            // 
+            // pbAdventeCandle2
+            // 
+            this.pbAdventeCandle2.Location = new System.Drawing.Point(262, 0);
+            this.pbAdventeCandle2.Name = "pbAdventeCandle2";
+            this.pbAdventeCandle2.Size = new System.Drawing.Size(100, 129);
+            this.pbAdventeCandle2.TabIndex = 2;
+            this.pbAdventeCandle2.TabStop = false;
+            // 
+            // pbAdventeCandle3
+            // 
+            this.pbAdventeCandle3.Location = new System.Drawing.Point(496, 216);
+            this.pbAdventeCandle3.Name = "pbAdventeCandle3";
+            this.pbAdventeCandle3.Size = new System.Drawing.Size(132, 112);
+            this.pbAdventeCandle3.TabIndex = 3;
+            this.pbAdventeCandle3.TabStop = false;
+            // 
+            // pbAdventeCandle4
+            // 
+            this.pbAdventeCandle4.Location = new System.Drawing.Point(262, 462);
+            this.pbAdventeCandle4.Name = "pbAdventeCandle4";
+            this.pbAdventeCandle4.Size = new System.Drawing.Size(100, 120);
+            this.pbAdventeCandle4.TabIndex = 4;
+            this.pbAdventeCandle4.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // frmAdvente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 578);
+            this.Controls.Add(this.pbAdventeCandle4);
+            this.Controls.Add(this.pbAdventeCandle3);
+            this.Controls.Add(this.pbAdventeCandle2);
+            this.Controls.Add(this.pbAdventeCandle1);
+            this.Controls.Add(this.pbAdvente);
+            this.Name = "frmAdvente";
+            this.Text = "Adventes kalendārs";
+            this.Load += new System.EventHandler(this.frmAdvente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdvente)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdventeCandle4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAdvente;
+        private System.Windows.Forms.PictureBox pbAdventeCandle1;
+        private System.Windows.Forms.PictureBox pbAdventeCandle2;
+        private System.Windows.Forms.PictureBox pbAdventeCandle3;
+        private System.Windows.Forms.PictureBox pbAdventeCandle4;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ frmAdvente.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdventesKalendars
+{
+    public partial class frmAdvente : Form
+    {
+        public frmAdvente()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAdvente_Load(object sender, EventArgs e)
+        {
+            DateTime dateNow = new DateTime();
+            dateNow = DateTime.Now;
+            int todayIs = int.Parse(dateNow.Day.ToString());
+
+            DateTime advent1 = new DateTime(2015, 11, 29);
+            DateTime advent2 = new DateTime(2015, 12, 6);
+            DateTime advent3 = new DateTime(2015, 12, 13);
+            DateTime advent4 = new DateTime(2015, 12, 20);
+
+            Console.WriteLine(todayIs);
+
+            if (dateNow >= advent1)
+            {
+                Console.WriteLine("Pirmā advente");
+                pbAdventeCandle1.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle1.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle1.Parent = pbAdvente;
+                pbAdventeCandle1.BackColor = Color.Transparent;
+                //pbAdventeCandle1.Location = new Point(0, 0);
+                //pbAdventeCandle1.BackColor = Color.Transparent;
+                pbAdventeCandle2.SendToBack();
+                pbAdventeCandle3.SendToBack();
+                pbAdventeCandle4.SendToBack();
+            }
+            
+            if (dateNow >= advent2)
+            {
+                Console.WriteLine("Otrā advente");
+                pbAdventeCandle1.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle1.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle1.Parent = pbAdvente;
+                pbAdventeCandle1.BackColor = Color.Transparent;
+
+                pbAdventeCandle2.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle2.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle2.Parent = pbAdvente;
+                pbAdventeCandle2.BackColor = Color.Transparent;
+
+                pbAdventeCandle3.SendToBack();
+                pbAdventeCandle4.SendToBack();
+            }
+            
+            if (dateNow >= advent3)
+            {
+                Console.WriteLine("Trešā advente");
+
+                pbAdventeCandle1.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle1.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle1.Parent = pbAdvente;
+                pbAdventeCandle1.BackColor = Color.Transparent;
+
+                pbAdventeCandle2.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle2.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle2.Parent = pbAdvente;
+                pbAdventeCandle2.BackColor = Color.Transparent;
+
+                pbAdventeCandle3.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle3.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle3.Parent = pbAdvente;
+                pbAdventeCandle3.BackColor = Color.Transparent;
+
+                pbAdventeCandle4.SendToBack();
+            }
+            
+            if (dateNow >= advent4)
+            {
+                Console.WriteLine("Ceturtā advente");
+
+                pbAdventeCandle1.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle1.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle1.Parent = pbAdvente;
+                pbAdventeCandle1.BackColor = Color.Transparent;
+
+                pbAdventeCandle2.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle2.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle2.Parent = pbAdvente;
+                pbAdventeCandle2.BackColor = Color.Transparent;
+
+                pbAdventeCandle3.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle3.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle3.Parent = pbAdvente;
+                pbAdventeCandle3.BackColor = Color.Transparent;
+
+                pbAdventeCandle4.Image = Properties.Resources.AdventeCandleReal;
+                pbAdventeCandle4.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAdventeCandle4.Parent = pbAdvente;
+                pbAdventeCandle4.BackColor = Color.Transparent;
+            }
+        }
+    }
+}
